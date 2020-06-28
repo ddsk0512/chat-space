4a07010dbdbab2266dadc9880814ef50fe82bd3a
ddsk0512 <65224155+ddsk0512@users.noreply.github.com>
2020-06-28 15:19:05 +0900

    L9-2-2commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, uniqueness: true
 
+  belongs_to :group
+  belongs_to :user
 end
