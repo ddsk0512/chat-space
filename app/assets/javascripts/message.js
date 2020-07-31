9940d8f1e7462724aa197771e1e31a2d621503a2
ddsk0512 <65224155+ddsk0512@users.noreply.github.com>
2020-07-31 17:54:46 +0900

    after deploy shuusei

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -9,7 +9,7 @@ $(function() {
                     ${message.user_name}
                   </div>
                   <div class="upper-message__date">
-                    ${message.date}
+                    ${message.created_at}
                   </div>
                 </div>
                 <div class="lower-message">
@@ -24,6 +24,31 @@ $(function() {
 
 
     //中略(非同期通信の記述)
+    $('#new_message').on('submit', function(e) {
+        e.preventDefault();
+        var formData = new FormData(this);
+        var url = $(this).attr('action')
+        $.ajax({
+                url: url,
+                type: "POST",
+                data: formData,
+                dataType: 'json',
+                processData: false,
+                contentType: false
+            })
+            .done(function(data) {
+                var html = buildHTML(data);
+                $('.messages').append(html);
+                $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+                $('.form__submit').prop('disabled', false);
+
+                $('form')[0].reset();
+            })
+            .fail(function() {
+                alert("メッセージ送信に失敗しました");
+                $('.form__submit').prop('disabled', false);
+            });
+    })
 
     $(function() {
         var reloadMessages = function() { //自動更新の関数
